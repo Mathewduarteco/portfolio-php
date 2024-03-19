36b5a6268124f058c5fe4802d5f6e41f2ea4d991
Matthias A. Duarte <matthalan.dev@gmail.com>
2024-03-18 21:08:37 -0500

    plant commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// selecting the html elements
-
-// activating the hamburger and mobile nav
-
-// hide mobile navigation after clicking a link or clicking outside
-
-// adding sticky animation to the navigation
-
-// mixitup for filtering the portfolio works
